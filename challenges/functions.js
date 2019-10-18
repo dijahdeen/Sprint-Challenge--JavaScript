572c4b9e37c9ddd29673a7f74ca2023a8e263f5e
Dijah Deen <dijah@iams-MacBook-Pro.local>
2019-10-18 14:02:03 -0400

    this is dijahs js sprint

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,26 +6,53 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function dividing(num1, num2, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(num1 / num2);
+}
 
+const printvalue = (num) => {
+  console.log(num);
+}
+dividing(10, 2, printvalue);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function consume(num1, num2, cb) {
+  return cb(num1, num2);
+}
+
+const add = (num1, num2) => {
+  return num1 + num2;
+}
+
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+console.log(consume(2, 5, add));
+console.log(consume(2, 5, multiply));
+
+function greeting(first_name, last_name, cb) {
+  return ("Hello " + first_name + " " + last_name + " nice to meet you!");
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: because it is nested within that variable and its inheriting the funtions defined in it.
 
 
 const external = "I'm outside the function";
